708e3dd2b84d962f96f417e69eca0cf77c4bf656
ayeUCSD <114363008+ayeUCSD@users.noreply.github.com>
2022-10-07 13:14:42 -0700

    started the pa

--- pa2-starter/src/ArrayGL.java
+++ pa2-starter/src/ArrayGL.java
@@ -2,11 +2,47 @@ public class ArrayGL<E> implements MyList<E> {
 
     E[] elements;
     int size;
+    int endInd;
 
     public ArrayGL(E[] initialElements) {
         this.elements = initialElements;
-        this.size = initialElements.length;
+        this.size= this.endInd = initialElements.length;
     }
 
+    
+    
+    public void add(E input) {
+        if(elements[endInd] != null) {
+            expandSize();
+        }
+    }
+    
+    
+    public void remove(E input) {}
+    
+    
+    /**
+     * doubles the size of the array
+     */
+    public void expandSize() {
+        E[] newArray = new E[2*size];
+    }
+    
     // Fill in all required methods here
+    
+    
+    public E[] toArray() {
+        
+    }
+    public void transformAll(MyTransformer mt) {
+        
+    }
+    public void chooseAll(MyChooser mc) {
+        
+    }
+    
+    
+    public boolean isEmpty() {
+        return elements[0] != null;
+    }
 }
