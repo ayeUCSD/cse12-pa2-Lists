cabe9d4c0b12139ad27995a8719f7debf3d6945d
ayeUCSD <114363008+ayeUCSD@users.noreply.github.com>
2022-10-09 17:14:15 -0700

    worked on linked and array

--- pa2-starter/src/ArrayGL.java
+++ pa2-starter/src/ArrayGL.java
@@ -1,45 +1,105 @@
-import java.util.Arrays;
+
 public class ArrayGL<E> implements MyList<E> {
 
     E[] elements;
     int size;
-    int endInd;
+    int index;
 
     public ArrayGL(E[] initialElements) {
         this.elements = initialElements;
-        this.size= this.endInd = initialElements.length;
+        this.size= this.index = initialElements.length - 1; //we want the index
     }
 
     
-    
-    public void add(E input) {
-        if(elements[endInd] != null) {
-            expandSize();
-        }
+    /**
+     * 
+     * @param input
+     * @return if we expanded the list or not
+     */
+    public boolean add(E input) {
+        elements[index] = input;
+         size++;
+         return setIndex();
+       
     }
     
     
-    public void remove(E input) {}
+    /**
+     * stupid linear search for a null index
+     * sets index to a spot with a null
+     * otherwise sets it to the end and doubles the size of the list
+     * 
+     * returns whether or not we expanded the list or not
+     */
+    public boolean setIndex() {
+    	for(int i = 0; i < size; i++) {
+    		if(elements[i] == null) {
+    			index = i;
+    			return false;
+    		}
+    	}
+    	index = elements.length;
+    	expandSize();
+    	return true;
+    }
     
+    public E remove(E input) {
+    	//we do a linear search for input :(
+    	//swap end with index value
+    	for(int i = 0; i < size; i++) {
+    		if(elements[i].equals(input)) {
+    			E output = elements[i];
+    			elements[i] = null;
+    			size--;
+    			return output;
+    		}
+    	}
+    	return null;
+    }
     
     /**
      * doubles the size of the array
      */
+    @SuppressWarnings("unchecked")
     public void expandSize() {
-        elements = Arrays.copyOf(elements, size*2); //(E[])new Object[2*size];
+        E[] copyArray = (E[]) new Object[2*size];
+        for(int i = 0; i < this.size; i++) {
+        	copyArray[i] = elements[i];
+        }
     }
     
     // Fill in all required methods here
-    
-    
+    /**
+     * returns an array in the same order without null spaces between entries
+     */
+    @SuppressWarnings("unchecked")
     public E[] toArray() {
+    	E[] output = (E[]) new Object[size];
+    	int i = 0;
+    	for(E temp : elements) {
+    		if(temp != null) {
+    			output[i++] = temp;
+    		}
+    	}
         
     }
+    
+    @SuppressWarnings("unchecked")
     public void transformAll(MyTransformer mt) {
-        
+        for(int i = 0; i < elements.length; i++) {
+        	if(elements[i] != null) {		
+        	elements[i] = (E) mt.transformElement(elements[i]);
+        	}
+        }
     }
     public void chooseAll(MyChooser mc) {
-        
+    	for(int i = 0; i < elements.length; i++) {
+        	if(!mc.chooseElement(elements[i])){
+        		elements[i] = null;
+        	}
+        }
+    	elements = toArray();
+        size = elements.length;
     }
     
     
--- pa2-starter/src/LinkedGL.java
+++ pa2-starter/src/LinkedGL.java
@@ -14,9 +14,54 @@ public class LinkedGL<E> implements MyList<E> {
     int size;
 
     public LinkedGL(E[] contents) {
-        // Fill in this constructor
+        this.front = new Node(contents[0], null);
+        Node temp = front;
+        for(int i = 1; i < contents.length; i++) {
+        	temp.next = new Node(contents[i],null);
+        	temp = temp.next;
+        }
+    }
+    
+    public void add(E input) {
+    	Node end = getEnd();
+    	end.next = new Node(input, null);
+    }
+    
+    public void remove(E input) {
+    	Node temp = front;
+    	Node previous = null;
+    	while(temp != null && !temp.next.value.equals(input)) {
+    		previous = temp;
+    		temp = temp.next;
+    	}
+    	if(temp == null) {
+    		return;
+    	}
+    	previous.next = temp.next;
     }
 
+    public Node getEnd() {
+    	Node temp = front;
+    	while(temp.next != null) {
+    		temp = temp.next;
+    	}
+    }
+    
     // Fill in all methods
-
+    // Fill in all required methods here
+    @SuppressWarnings("unchecked")
+    public E[] toArray() {
+    	return null;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public void transformAll(MyTransformer mt) {
+    }
+    public void chooseAll(MyChooser mc) {
+    }
+    
+    
+    public boolean isEmpty() {
+    	return false;
+    }
 }
